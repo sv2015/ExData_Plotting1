c15d673141fff3d47db2c43f69cb6fb16a862f6f
Sushma Sanketh <sushma.venkatesh@gmail.com>
2017-02-04 10:38:16 -0600

    Revert "Revert "Programming Assignment""
    This reverts commit 0fe5bca0026e3f0ccca96484456f06df775daf7f.

---
+++ plot3.R
@@ -0,0 +1,42 @@
+# Create data folder
+if(!file.exists("data")) {
+  dir.create("data")
+}
+
+# download zip file
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileUrl, destfile="./data/electric.zip", mode="wb")
+dateDownloadedEPC <- date()
+# [1] "Sat Feb 04 01:22:27 2017"
+
+# unzip and read table
+unzip(zipfile = "./data/electric.zip", exdir = "./data")
+
+library(sqldf)
+library(dplyr)
+library(gsubfn)
+epc <- read.table("./data/household_power_consumption.txt", 
+                  sep = ";", header = TRUE, stringsAsFactors = FALSE, na.strings = "?")
+
+# format date
+epc$Date <- as.Date(epc$Date, format="%d/%m/%Y")
+
+# filter the dataset
+d1 <- as.Date("2007-02-01")
+d2 <- as.Date("2007-02-02")
+
+epcsub <- subset(epc, Date %in% c(d1, d2))
+
+# Create datetime stamp
+epcsub$DateTime <- as.POSIXct(paste(epcsub$Date, epcsub$Time), format="%Y-%m-%d %H:%M:%S")
+
+# plot 3
+png(filename="plot3.png", width = 480, height = 480)
+
+plot(epcsub$DateTime, epcsub$Sub_metering_1, xlab = "", ylab = "Energy sub metering", type = "l", col = "black")
+lines(epcsub$DateTime, epcsub$Sub_metering_2, col = "red")
+lines(epcsub$DateTime, epcsub$Sub_metering_3, col = "blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lwd = 2, col = c("black", "red", "blue"))
+
+dev.off()
+
